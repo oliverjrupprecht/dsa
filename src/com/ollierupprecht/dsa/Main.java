b7a215a6e21e6ccf53bf38c5c3d83bdba7968934
Oliver Rupprecht <oliverjrupprecht@gmail.com>
2025-06-17 21:57:59 +0100

    add InsertionSort

--- src/com/ollierupprecht/dsa/Main.java
+++ src/com/ollierupprecht/dsa/Main.java
@@ -2,12 +2,13 @@ package com.ollierupprecht.dsa;
 
 import com.ollierupprecht.dsa.algorithms.searching.*;
 import com.ollierupprecht.dsa.algorithms.sorting.BubbleSort;
+import com.ollierupprecht.dsa.algorithms.sorting.InsertionSort;
 
 public class Main {
     public static void main(String[] args) {
         int[] arr = {9,8,7,6,5};
 
-        System.out.println(BinarySearch.searchUnsorted(arr, 9));
+        InsertionSort.sort(arr);
 
         for (int i: arr) {
             System.out.println(i);
--- src/com/ollierupprecht/dsa/algorithms/sorting/InsertionSort.java
+++ src/com/ollierupprecht/dsa/algorithms/sorting/InsertionSort.java
@@ -1,4 +1,19 @@
 package com.ollierupprecht.dsa.algorithms.sorting;
 
 public class InsertionSort {
+    public static void sort(int[] arr) {
+        // I low-key think that it should be called shift sort because it shifts up numbers greater than the key until it can insert
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i]; // current element being inserted
+            int partition = i - 1; // index of sorted partition
+
+            // shift along every element that is greater than the key
+            while (partition >= 0 && arr[partition] > key) {
+                arr[partition + 1] = arr[partition];
+                partition--;
+            }
+
+            arr[partition + 1] = key; // insert the key into the array at correct position
+        }
+    }
 }
