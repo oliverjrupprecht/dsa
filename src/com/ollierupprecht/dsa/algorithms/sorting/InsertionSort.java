d39df6699beb430e4ffda1fef1f6c5454952d35e
Oliver Rupprecht <oliverjrupprecht@gmail.com>
2025-06-18 16:54:13 +0100

    added comments to InsertionSort implementation

--- src/com/ollierupprecht/dsa/algorithms/sorting/InsertionSort.java
+++ src/com/ollierupprecht/dsa/algorithms/sorting/InsertionSort.java
@@ -1,19 +1,30 @@
 package com.ollierupprecht.dsa.algorithms.sorting;
 
 public class InsertionSort {
-    public static void sort(int[] arr) {
-        // I low-key think that it should be called shift sort because it shifts up numbers greater than the key until it can insert
-        for (int i = 1; i < arr.length; i++) {
-            int key = arr[i]; // current element being inserted
-            int partition = i - 1; // index of sorted partition
+    /*
+    The insertion sort algorithm iterates up the array, selecting a new key each iteration. This key is compared
+    with each element in the sorted portion of the array, if the element is greater than it is shifted up an index.
+    Once an element is less than the key, the key is inserted into the array.
 
-            // shift along every element that is greater than the key
-            while (partition >= 0 && arr[partition] > key) {
-                arr[partition + 1] = arr[partition];
-                partition--;
+    I low-key think that it should be called shift sort because I believe it better describes the algorithm behavior.
+
+     Worst case time performance is O(n^2), best case is O(n). It performs better than bubble sort in the real world,
+     because its more performant on nearly sorted arrays.
+     */
+    public static void sort(int[] arr) {
+
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i]; // element to be inserted
+            int j = i - 1; // j init as the largest sorted index
+
+            // while sorted element is greater than the key, shift it up
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
             }
 
-            arr[partition + 1] = key; // insert the key into the array at correct position
+            // once while loop is broken the key can be inserted
+            arr[j + 1] = key;
         }
     }
 }
